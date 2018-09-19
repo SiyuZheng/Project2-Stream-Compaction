534a24c2d54be1b6fca5823b8dfef95ba79ceefa
SiyuZheng <siyuz@seas.upenn.edu>
2018-09-18 23:16:03 -0400

    shared memory halfway

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -3,6 +3,15 @@
 #include "common.h"
 #include "efficient.h"
 #include "device_launch_parameters.h"
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/scan.h>
+#include "thrust.h"
+
+#define NUM_BANKS 16
+#define LOG_NUM_BANKS 4
+#define CONFLICT_FREE_OFFSET(n) \
+    ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))
 
 namespace StreamCompaction {
     namespace Efficient {
@@ -42,11 +51,8 @@ namespace StreamCompaction {
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        void nonoptscan(int n, int *odata, const int *idata) {
-            
+        void nonoptscan(int n, int *odata, const int *idata) {         
             // TODO
-			
-			
 			int upLimit = ilog2ceil(n);
 			int len = 1 << upLimit;
 
@@ -79,9 +85,7 @@ namespace StreamCompaction {
 			cudaMemcpy(odata, dev_data, len * sizeof(int), cudaMemcpyDeviceToHost);
 			checkCUDAError("cudaMemcpy dev_data failed!");
 			cudaFree(dev_data);
-			cudaDeviceSynchronize();
-
-            
+			cudaDeviceSynchronize();            
         }
 
 		__global__ void kernOptUpSweep(int n, int d, int *odata) {
@@ -166,7 +170,7 @@ namespace StreamCompaction {
 			}
 		}
 
-		__global__ void kernSharedMemoryScan(int n, int len, int *dev_odata, const int *dev_idata, int *OriRoot) {
+		__global__ void kernSharedMemoryScan(int n, int *odata, const int *idata, int *blockData) {
 			int index = (blockDim.x * blockIdx.x) + threadIdx.x;
 			if (index >= n) {
 				return;
@@ -174,28 +178,31 @@ namespace StreamCompaction {
 			extern __shared__ int temp[];
 			int thid = threadIdx.x;
 			int offset = 1;
-			int blockOffset = blockDim.x * blockIdx.x;
-			temp[thid] = dev_idata[blockOffset + thid];
-			//temp[2 * thid] = dev_idata[2 * thid]; // load input into shared memory
-			//temp[2 * thid + 1] = dev_idata[2 * thid + 1];
-			for (int d = len >> 1; d > 0; d >>= 1)
+			int stride = blockIdx.x * blockDim.x;
+			int ai = thid;
+			int bi = thid + (n / 2);
+			int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
+			int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+			temp[ai + bankOffsetA] = idata[ai + stride];
+			temp[bi + bankOffsetB] = idata[bi + stride];
+			for (int d = n >> 1; d > 0; d >>= 1)
 			{
 				__syncthreads();
 				if (thid < d)
 				{
 					int ai = offset*(2 * thid + 1) - 1;
 					int bi = offset*(2 * thid + 2) - 1;
+					ai += CONFLICT_FREE_OFFSET(ai);
+					bi += CONFLICT_FREE_OFFSET(bi);
 					temp[bi] += temp[ai];
 				}
 				offset *= 2;
 			}
 			if (thid == 0) { 
-				if (thid == 0) {
-					OriRoot[blockIdx.x] = temp[len - 1];
-					temp[len - 1] = 0;
-				}
+				blockData[blockIdx.x] = temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)];
+				temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0; 
 			}
-			for (int d = 1; d < len; d *= 2) // traverse down tree & build scan
+			for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
 			{
 				offset >>= 1;
 				__syncthreads();
@@ -203,56 +210,66 @@ namespace StreamCompaction {
 				{
 					int ai = offset*(2 * thid + 1) - 1;
 					int bi = offset*(2 * thid + 2) - 1;
+					ai += CONFLICT_FREE_OFFSET(ai);
+					bi += CONFLICT_FREE_OFFSET(bi);
 					float t = temp[ai];
 					temp[ai] = temp[bi];
 					temp[bi] += t;
 				}
 			}
-			dev_odata[blockOffset + thid] = temp[thid];
-			//dev_odata[2 * thid] = temp[2 * thid]; // write results to device memory
-			//dev_odata[2 * thid + 1] = temp[2 * thid + 1];
+			odata[ai + stride] = temp[ai + bankOffsetA];
+			odata[bi + stride] = temp[bi + bankOffsetB];
 		}
 
-		__global__ void kernAddOriRoot(int N, int* OriRoot, int* dev_odata) {
-			int index = threadIdx.x + (blockIdx.x * blockDim.x);
-			if (index >= N) {
+		__global__ void kernAddBlockData(int n, int *odata, const int *idata, const int *blockData) {
+			int index = (blockDim.x * blockIdx.x) + threadIdx.x;
+			if (index >= n) {
 				return;
 			}
-			dev_odata[index] += OriRoot[blockIdx.x];
+			odata[index] = idata[index] + blockData[blockIdx.x];
 		}
 
-		//void sharedMemoryScan(int n, int *odata, const int *idata) {
-		//	int upLimit = ilog2ceil(n);
-		//	int len = 1 << upLimit;
+		void sharedMemoryScan(int n, int *odata, const int *idata) {
+			int upLimit = ilog2ceil(n);
+			int len = 1 << upLimit;
 
-		//	int blockSize = 1024;
-		//	dim3 threadsPerBlock(blockSize);
-		//	dim3 blocksPerGrid((len + blockSize - 1) / blockSize);
-		//	int partSize = 1 << (ilog2ceil((len + blockSize - 1) / blockSize));
-		//	int *dev_idata;
-		//	int *dev_odata;
-		//	int *old_data;
-		//	cudaMalloc((void**)&dev_idata, len * sizeof(int));
-		//	checkCUDAError("cudaMalloc dev_idata failed!");
-		//	cudaMalloc((void**)&dev_odata, len * sizeof(int));
-		//	checkCUDAError("cudaMalloc dev_odata failed!");
-		//	cudaMalloc((void**)&old_data, partSize * sizeof(int));
-		//	checkCUDAError("cudaMalloc old_data failed!");
-		//	cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
-		//	checkCUDAError("cudaMemcpy dev_idata failed!");
+			int blockSize = 64;
+			dim3 threadsPerBlock(blockSize);
+			dim3 blocksPerGrid((len + blockSize - 1) / blockSize);
+			int partSize = (len + blockSize - 1) / blockSize;
+			int *dev_idata;
+			int *dev_odata;
+			int *dev_blockData;
+			int *dev_blockSum;
+			cudaMalloc((void**)&dev_idata, len * sizeof(int));
+			checkCUDAError("cudaMalloc dev_idata failed!");
+			cudaMalloc((void**)&dev_odata, len * sizeof(int));
+			checkCUDAError("cudaMalloc dev_odata failed!");
+			cudaMalloc((void**)&dev_blockData, partSize * sizeof(int));
+			checkCUDAError("cudaMalloc dev_blockData failed!");
+			cudaMalloc((void**)&dev_blockSum, partSize * sizeof(int));
+			checkCUDAError("cudaMalloc dev_blockSum failed!");
+			cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+			checkCUDAError("cudaMemcpy dev_idata failed!");
 
 
 
-		//	timer().startGpuTimer();
-		//	kernSharedMemoryScan << <blocksPerGrid, threadsPerBlock >> > (len, dev_odata, dev_idata);
-		//	timer().endGpuTimer();
-		//	cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
-		//	checkCUDAError("cudaMemcpy dev_odata failed!");
+			timer().startGpuTimer();
+			kernSharedMemoryScan << <blocksPerGrid, threadsPerBlock >> > (len, dev_odata, dev_idata, dev_blockData);
+			checkCUDAError("kernSharedMemoryScan failed!");
+			//thrust::inclusive_scan(dev_blockData, dev_blockData + n, dev_blockSum);
+			//kernAddBlockData << <blocksPerGrid, threadsPerBlock >> > (len, dev_odata, dev_idata, dev_blockSum);
+			//checkCUDAError("kernSharedMemoryScan failed!");
+			timer().endGpuTimer();
+			cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+			checkCUDAError("cudaMemcpy dev_odata failed!");
 
-		//	cudaFree(dev_idata);
-		//	cudaFree(dev_odata);
-		//	cudaDeviceSynchronize();
-		//}
+			cudaFree(dev_idata);
+			cudaFree(dev_odata);
+			cudaFree(dev_blockData);
+			cudaFree(dev_blockSum);
+			cudaDeviceSynchronize();
+		}
 
 
 
